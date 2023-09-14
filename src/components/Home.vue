942aeccaf8a03e28f4e46cf8e8edcf3c216f101b
Elias <mail@elias-dieter.de>
2023-09-14 12:01:25 +0200

    update all Items If Item Added

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,21 +1,43 @@
 <template>
   <div>
     Hallo das ist ein Rechner
-    <AddEntrie></AddEntrie>
+    <AddEntrie @update-entries="updateEntries"/>
+    <div v-if="cookieValue !== null" v-for="item in cookieValue">
+      <Item :item="item"/>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import AddEntrie from './AddEntrie.vue';
+import Entry from '../Models/Entry';
+import Item from './Item.vue';
+
+interface cookie {
+  cookieValue: null | Entry[];
+}
+
 
 export default defineComponent({
   name: 'Home',
   components: {
-    AddEntrie
+    AddEntrie,
+    Item
   },
-  mounted(){
-    
+  data(): cookie {
+    return {
+      cookieValue: null,
+    };
+  },
+  mounted() {
+    this.cookieValue = JSON.parse(localStorage.getItem('myCookie') || 'null');
+  },
+  methods: {
+    updateEntries() {
+      console.log("Okay");
+      this.cookieValue = JSON.parse(localStorage.getItem('myCookie') || 'null');
+    },
   }
 });
 </script>
